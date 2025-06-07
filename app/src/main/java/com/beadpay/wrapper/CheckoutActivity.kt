2e64b09b90a16965b858697fa2e3557755483abc
Marc Castrechini <marc@castro9.com>
2025-06-07 08:01:00 -0400

    chore: add stub CheckoutActivity to unblock compile

---
+++ app/src/main/java/com/beadpay/wrapper/CheckoutActivity.kt
@@ -0,0 +1,14 @@
+﻿package com.beadpay.wrapper
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/** Temporary stub; real WebView logic will be added later. */
+class CheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // No UI yet – just end immediately
+        setResult(RESULT_CANCELED)
+        finish()
+    }
+}
